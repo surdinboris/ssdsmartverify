cbec55709ca4e4127adbcca716ec4133c506bfe2
borrik <borrik@live.ru>
2019-10-31 16:11:17 +0200

    user output minor change

--- consist.py
+++
@@ -1,6 +0,0 @@
-ssds=[{'device': '/dev/sda', 'slot': '0', 'vendor': 'Hitachi', 'model': 'HUA72101'}]
-
-for ssd in ssds:
-    print("Slot: {}  Model: {}".format(ssd["slot"], ssd["model"], ))
-
-dd={'Hitachi HUA72101': '0'}
